a85e85b2becddc2dbccb70598aa12d80d0b09453
prim <prim>
2020-03-13 23:16:49 +0800

    nt

---
+++ Spring/t_spring-aop/src/main/java/com/prim/UserDaoImpl.java
@@ -0,0 +1,31 @@
+package com.prim;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public void save() {
+        //保存用户
+    }
+
+    @Override
+    public void update() {
+        //更新信息
+    }
+
+    @Override
+    public void delete() {
+        //删除信息
+    }
+
+    @Override
+    public void find() {
+        //查找信息
+    }
+
+    /**
+     * 检查权限的方法
+     * 比如：要对所有对DAO方法进行权限校验
+     */
+    public void checkPrivilege(){
+
+    }
+}
